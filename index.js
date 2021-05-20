96830833eafbd4f6678f1027e5eabee5a6ec65f7
xiaobai <xiaobai-world@foxmail.com>
2021-05-20 23:48:29 +0800

    add road

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ console.log('hello world')
 const pkg = require('./package.json')
 console.log(pkg)
 console.log(process.env.PWD)
+
+
+/**
+ * 1. read dist, and push to xiaobai-backend.
+ * 2. on pushed to xiaobai-backend, save file to rand directory.
+ * 3. vite project must have "store" field,example:
+ * ```
+ * store: {
+ *    name: "xxx",
+ *    id: "xxx",
+ *    icon: "xxx",
+ *    git: "git registry, optional"
+ * }
+ * ```
+ */
